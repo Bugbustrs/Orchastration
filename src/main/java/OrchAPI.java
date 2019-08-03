219a9e5e8c5c382f4060dee07365424569e92ac1
David Kheri <Davidf.kheri@gmail.com>
2019-08-03 22:56:04 +0200

    Made OrchAPI contain static Members

--- src/main/java/OrchAPI.java
+++ src/main/java/OrchAPI.java
@@ -4,27 +4,22 @@ import java.util.concurrent.Callable;
 
 public class OrchAPI {
 
-    final String MEASUREMENT_REQUEST_TYPE="SCHEDULE_MEASUREMENT";
-    final String MEASUREMENT_CHECK_IN_TYPE="checkIn";
-    final String MEASUREMENT_SUCCESSFUL="MeasurementSucceed";
+    private final static String MEASUREMENT_SCHEDULE="SCHEDULE_MEASUREMENT";
+    private final static String MEASUREMENT_CHECK_IN_TYPE="CHECKIN";
+    private final static String MEASUREMENT_SUCCESSFUL="MEASUREMENT_SUCCESSFUL";
 
-    public OrchAPI() {
-
-    }
-
-    public Object returnResponse(JSONObject request){
+    public static Object returnResponse(JSONObject request){
        String requestType = (String)request.get("request_type");
-       Object response=null; //dont know if ts a string or a json object is needed as of yet?
-       if(requestType.equalsIgnoreCase(MEASUREMENT_CHECK_IN_TYPE)){
+       Object response=null;
+       if(requestType.equals(MEASUREMENT_CHECK_IN_TYPE)){
            //send the client a list of available jobs;
            response=Measurement.getActiveJobs();
+           return response;
        }
-       else if(requestType.equals(MEASUREMENT_REQUEST_TYPE)){
+       else if(requestType.equals(MEASUREMENT_SCHEDULE)){
             //the request contains Measurement Description so should be added to the list of jobs
             Measurement.addMeasurement(request);
-            return null; //or generate success response?;
        }
-
        else if(requestType.equals(MEASUREMENT_SUCCESSFUL)) {
            return Measurement.recordSuccessfulJob(request);
        }
